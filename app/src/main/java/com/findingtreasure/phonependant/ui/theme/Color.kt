08bcbc6a2939e803225ad3aa38f36b2ab46d1e80
ninokiers <nino.kiers0006@stud.hkr.se>
2024-10-22 15:53:33 +0200

    Created theme

--- app/src/main/java/com/findingtreasure/phonependant/ui/theme/Color.kt
+++ app/src/main/java/com/findingtreasure/phonependant/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.findingtreasure.phonependant.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Teal = Color(0xFF174A5B)
+val Orange = Color(0xFFFAB37D)
+val Purple = Color(0xFF908FA2)
+val White = Color(0xFFE1E1E1)
+val Black = Color(0xF01F1F1F)
+val Red = Color(0xFFFF5449)
--- app/src/main/java/com/findingtreasure/phonependant/ui/theme/Type.kt
+++ app/src/main/java/com/findingtreasure/phonependant/ui/theme/Type.kt
@@ -2,33 +2,44 @@ package com.findingtreasure.phonependant.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.findingtreasure.phonependant.R
 
-// Set of Material typography styles to start with
+// Define the Poppins font family
+val Poppins = FontFamily(
+	Font(R.font.poppins_regular, FontWeight.Normal),
+	Font(R.font.poppins_semibold, FontWeight.SemiBold),
+	Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
+// Define the typography styles
 val Typography = Typography(
+	titleLarge = TextStyle(
+		fontFamily = Poppins,
+		fontWeight = FontWeight.SemiBold,
+		fontSize = 40.sp  // Title large - 40pt
+	),
+	titleSmall = TextStyle(
+		fontFamily = Poppins,
+		fontWeight = FontWeight.SemiBold,
+		fontSize = 25.sp  // Title small - 25pt
+	),
 	bodyLarge = TextStyle(
-		fontFamily = FontFamily.Default,
+		fontFamily = Poppins,
 		fontWeight = FontWeight.Normal,
-		fontSize = 16.sp,
-		lineHeight = 24.sp,
-		letterSpacing = 0.5.sp
+		fontSize = 20.sp  // Body large - 20pt
+	),
+	labelLarge = TextStyle(
+		fontFamily = Poppins,
+		fontWeight = FontWeight.Normal,
+		fontSize = 16.sp  // Label large - 16pt
+	),
+	bodySmall = TextStyle(
+		fontFamily = Poppins,
+		fontWeight = FontWeight.Normal,
+		fontSize = 12.sp  // Body small - 12pt
 	)
-	/* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
