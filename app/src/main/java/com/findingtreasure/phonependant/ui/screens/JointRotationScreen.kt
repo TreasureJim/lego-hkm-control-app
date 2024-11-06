621d843e8993df8eee017187411432d32a4917b1
esz366 <esztii.kalmar@gmail.com>
2024-11-06 18:31:58 +0100

    Modified Joint Rotation screen to have new slider and show coordinates / joint rotations.
    Signed-off-by: ninokiers <nino.kiers0006@stud.hkr.se>

--- app/src/main/java/com/findingtreasure/phonependant/ui/screens/JointRotationScreen.kt
+++ app/src/main/java/com/findingtreasure/phonependant/ui/screens/JointRotationScreen.kt
@@ -2,7 +2,6 @@ package com.findingtreasure.phonependant.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
@@ -12,13 +11,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import com.findingtreasure.phonependant.model.Position
+import com.findingtreasure.phonependant.ui.helper.DisplayField
+import com.findingtreasure.phonependant.ui.helper.AxisSlider
 
 @Composable
 fun JointRotationScreen(
@@ -29,7 +25,7 @@ fun JointRotationScreen(
     var slider1Value by remember { mutableStateOf(0f) }
     var slider2Value by remember { mutableStateOf(0f) }
     var slider3Value by remember { mutableStateOf(0f) }
-    var positionName by remember { mutableStateOf(position?.name ?: "") }
+    var positionState by remember { mutableStateOf(position) }
 
     // Main container
     Column(
@@ -47,10 +43,10 @@ fun JointRotationScreen(
             Tab(selected = true, onClick = { /* Stay on Joint screen */ }) {
                 Text("Joint", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.labelSmall)
             }
-            Tab(selected = false, onClick = { onTabSelected("coordinateInput/${position?.id}", position!!.copy(name = positionName)) }) {
+            Tab(selected = false, onClick = { onTabSelected("coordinateInput/${position?.id}", position!!.copy(name = positionState?.name ?: "")) }) {
                 Text("Coordinate", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.labelSmall)
             }
-            Tab(selected = false, onClick = { onTabSelected("accelerometerInput/${position?.id}", position!!.copy(name = positionName)) }) {
+            Tab(selected = false, onClick = { onTabSelected("accelerometerInput/${position?.id}", position!!.copy(name = positionState?.name ?: "")) }) {
                 Text("Accelerometer", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.labelSmall)
             }
         }
@@ -72,14 +68,6 @@ fun JointRotationScreen(
 
             Spacer(modifier = Modifier.height(24.dp))
 
-            // Axis Labels and Sliders
-            Text(
-                text = "Axis",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onBackground,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-
             Spacer(modifier = Modifier.height(16.dp))
 
             // Sliders in a Row
@@ -94,7 +82,33 @@ fun JointRotationScreen(
                 AxisSlider(label = "3", sliderValue = slider3Value, onValueChange = { slider3Value = it })
             }
 
-            Spacer(modifier = Modifier.weight(1f))
+            // DisplayFields for Joint Values and Coordinates in Two Columns
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                // Column for Joint Values
+                Column(horizontalAlignment = Alignment.Start) {
+                    DisplayField(label = "1", value = position?.axis1 ?: "0.0")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    DisplayField(label = "2", value = position?.axis2 ?: "0.0")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    DisplayField(label = "3", value = position?.axis3 ?: "0.0")
+                }
+
+                // Column for Coordinate Values
+                Column(horizontalAlignment = Alignment.Start) {
+                    DisplayField(label = "X", value = position?.x ?: "0.0")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    DisplayField(label = "Y", value = position?.y ?: "0.0")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    DisplayField(label = "Z", value = position?.z ?: "0.0")
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))  // Spacer to push content upwards
 
             // Position Name Input Field
             Row(
@@ -106,8 +120,10 @@ fun JointRotationScreen(
             ) {
                 // Transparent TextField with custom underline
                 BasicTextField(
-                    value = positionName,
-                    onValueChange = { positionName = it },
+                    value = positionState?.name ?: "",
+                    onValueChange = { newName ->
+                        positionState = positionState!!.copy(name = newName)
+                    },
                     textStyle = MaterialTheme.typography.bodyLarge.copy(color = Color.White),
                     modifier = Modifier
                         .weight(1f)
@@ -135,7 +151,7 @@ fun JointRotationScreen(
 
                 // Edit Icon Button
                 IconButton(
-                    onClick = { onSave(position!!.copy(name = positionName)) },
+                    onClick = { onSave(positionState!!) },
                     modifier = Modifier.size(48.dp)
                 ) {
                     Icon(
@@ -149,60 +165,5 @@ fun JointRotationScreen(
     }
 }
 
-@Composable
-fun AxisSlider(label: String, sliderValue: Float, onValueChange: (Float) -> Unit) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .padding(8.dp)
-    ) {
-        // Label Circle
-        Box(
-            modifier = Modifier
-                .size(32.dp)
-                .background(MaterialTheme.colorScheme.primary, CircleShape),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = label,
-                style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.onPrimary,
-                textAlign = TextAlign.Center
-            )
-        }
 
-        Spacer(modifier = Modifier.height(32.dp))
 
-        // Slider
-        Slider(
-            value = sliderValue,
-            onValueChange = onValueChange,
-            Modifier
-                .graphicsLayer {
-                    rotationZ = 270f
-                    transformOrigin = TransformOrigin(0f, 0f)
-                }
-                .layout { measurable, constraints ->
-                    val placeable = measurable.measure(
-                        Constraints(
-                            minWidth = constraints.minHeight,
-                            maxWidth = constraints.maxHeight,
-                            minHeight = constraints.minWidth,
-                            maxHeight = constraints.maxHeight,
-                        )
-                    )
-                    layout(placeable.height, placeable.width) {
-                        placeable.place(-placeable.width, 0)
-                    }
-                }
-                .width(280.dp)
-                .height(50.dp),
-            colors = SliderDefaults.colors(
-                thumbColor = MaterialTheme.colorScheme.primary,
-                activeTrackColor = MaterialTheme.colorScheme.primary,
-                inactiveTrackColor = MaterialTheme.colorScheme.surface
-            ),
-            valueRange = 0f..100f
-        )
-    }
-}
