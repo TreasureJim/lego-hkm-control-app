7654e37dcaed1887b4aedb248f64f2900e9b16cd
Liam Boreback <boreback.work@gmail.com>
2024-11-08 09:50:23 +0100

    Hardcode sens settings

--- app/src/main/java/com/findingtreasure/phonependant/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/findingtreasure/phonependant/viewmodel/SettingsViewModel.kt
@@ -9,22 +9,22 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class SettingsViewModel(private val dataStore: SettingsDataStore) : ViewModel() {
-    private val _jointsensitivity = MutableStateFlow(1f)
+    private val _jointsensitivity = MutableStateFlow((1f / 100f * 3.14f / 30f))
     val jointsensitivity: StateFlow<Float> = _jointsensitivity
 
-    private val _coordsensitivity = MutableStateFlow(1f)
+    private val _coordsensitivity = MutableStateFlow((1f / 100f * 5))
     val coordsensitivity: StateFlow<Float> = _coordsensitivity
 
-    private val _commandSendHertz = MutableStateFlow(100)
+    private val _commandSendHertz = MutableStateFlow(3)
     val commandSendHertz: StateFlow<Int> = _commandSendHertz
 
     init {
         viewModelScope.launch {
-            _jointsensitivity.value = (1f / 100f * 3.14f / 10f)
-            _coordsensitivity.value = (1f / 100f * 5)
+//            _jointsensitivity.value = (1f / 100f * 3.14f / 10f)
+//            _coordsensitivity.value = (1f / 100f * 5)
 //            _jointsensitivity.value = dataStore.jointSensitivity.first()
 //            _coordsensitivity.value = dataStore.coordSensitivity.first()
-            _commandSendHertz.value = dataStore.commandSendHertz.first()
+//            _commandSendHertz.value = dataStore.commandSendHertz.first()
         }
     }
 
