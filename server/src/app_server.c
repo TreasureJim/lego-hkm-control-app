a63c23977dbbc7e885af90384aeeed0979fc1714
Liam Boreback <boreback.work@gmail.com>
2024-11-16 22:35:41 +0100

    App server refactor

--- server/src/app_server.c
+++ server/src/app_server.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 enum S_ID { S_ID_MOTIONID = 0, S_ID_MOVEJOG, S_ID_ROBOTREQUESTSTATUS, S_ID_ROBOTSTATUS, S_ID_MOVELINEAR, S_ID_MOVEPOS };
@@ -17,57 +18,62 @@ void send_message(int sockfd, uint8_t* buffer, size_t length) {
 
 ssize_t receive_message(int sockfd, uint8_t* buffer, size_t length) {
   int received = recv(sockfd, buffer, length, 0);
-  if (received < length) {
+  if (received <= 0) {
 	fprintf(stderr, "[ERROR] reached error or EOF for app\n");
-	return -1;
+	return received;
   }
   return received;
 }
 
 int app_decode(int app_fd) {
-  while (true) {
+  ssize_t status = 1;
+  uint8_t* buf;
+  struct robotrequeststatus request_status_str;
+
+  while (status > 0) {
 	char sig_id;
 	recv(app_fd, &sig_id, 1, 0);
 
-	char* buf;
 
 	switch (sig_id) {
 	case S_ID_MOVEJOG:
 	  buf = malloc(sizeof(struct movejog));
-	  if (receive_message(app_fd, buf, sizeof(struct movejog)) < 0) {
-		return -1;
+	  if ((status = receive_message(app_fd, buf, sizeof(struct movejog))) < 0) {
+		break;
 	  }
 
 	  robot_send_movejog((struct movejog*)buf);
 
 	  free(buf);
+	  buf = NULL;
 	  break;
 
 	case S_ID_ROBOTREQUESTSTATUS:
-	  struct robotrequeststatus rqs;
-	  robot_send_robotrequeststatus((struct robotrequeststatus*)&rqs);
+	  robot_send_robotrequeststatus(&request_status_str);
 	  break;
 
 	case S_ID_MOVELINEAR:
 	  buf = malloc(sizeof(struct movelinear));
-	  if (receive_message(app_fd, buf, sizeof(struct movelinear)) < 0) {
-		return -1;
+	  if ((status = receive_message(app_fd, buf, sizeof(struct movelinear))) < 0) {
+		break;
 	  }
 
 	  robot_send_movelinear((struct movelinear*)buf);
 
 	  free(buf);
+	  buf = NULL;
 	  break;
 
 	case S_ID_MOVEPOS:
 	  buf = malloc(sizeof(struct movepos));
-	  if (receive_message(app_fd, buf, sizeof(struct movepos)) < 0) {
-		return -1;
+	  if ((status = receive_message(app_fd, buf, sizeof(struct movepos))) < 0) {
+		break;
 	  }
 
 	  robot_send_movepos((struct movepos*)buf);
 
 	  free(buf);
+	  buf = NULL;
 	  break;
 
 	default:
@@ -75,6 +81,12 @@ int app_decode(int app_fd) {
 	  break;
 	}
   }
+  
+  if (buf != NULL) {
+    free(buf);
+  }
+
+  return status;
 }
 
 void app_send_motionid(struct motionid* m) {
